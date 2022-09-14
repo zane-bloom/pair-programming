937ef34783bffe4cc4d862492449bf6cd15611ba
Zane Bloom <zane.bloom@vector.co.nz>
2022-09-14 12:42:16 +1200

    init

---
+++ src/main/java/nz/co/entelect/Main.java
@@ -0,0 +1,7 @@
+package nz.co.entelect;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/test/java/nz/co/entelect/RomanNumeralConverterTest.java
@@ -0,0 +1,15 @@
+package nz.co.entelect;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class RomanNumeralConverterTest {
+
+    @Test
+    public void test() {
+        assertThat(1, is(1));
+    }
+
+}
